26130f83fb8373cab3562534b86e203807836e01
abdon2018 <abdonsanchezuno@gmail.com>
2022-06-11 16:20:04 -0300

    se cambio version de java a 11

--- api_tienda/src/main/java/com/ecommerce/api/api_tienda/ApiTiendaApplication.java
+++ api_tienda/src/main/java/com/ecommerce/api/api_tienda/ApiTiendaApplication.java
@@ -1,18 +1,13 @@
 package com.ecommerce.api.api_tienda;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiTiendaApplication implements CommandLineRunner {
+public class ApiTiendaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTiendaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("hola cafe");
-	}
 }
